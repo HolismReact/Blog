2c933304bccfae8f62e40d0f3e25e455746c7247
“Sanam” <pourmand2010@users.noreply.github.com>
2021-12-16 12:53:20 +0330

    committed by script

--- Admin/Create.js
+++ Admin/Create.js
@@ -7,17 +7,6 @@ const fields =
             placeholder='Title'
             required='Please write the title.'
         />
-        <Enum
-            column='priorityId'
-            entity='priority'
-            placeholder='Priority'
-            required='Please choose the priority for this blog. Choose less importance if it is less urgent.'
-        />
-        <LongText
-            column='body'
-            placeholder='Please describe the problem!'
-            required='We need to know the problem to be able to help.'
-        />
     </>
 
 const CreateBlog = (props) => {
--- Admin/List.js
+++ Admin/List.js
@@ -2,99 +2,14 @@ import {
     List,
     Text,
     Enum,
-    ListAction,
-    ItemAction,
-    post,
-    ValueWithTitle,
-    app
 } from '@List';
-import DoneIcon from '@mui/icons-material/Done';
-import MessageIcon from '@mui/icons-material/Message';
 import CreateBlog from './Create';
 
-const filters =
-    <>
-        <Text column='title' placeholder='Title' />
-        <Enum column='stateId' placeholder='State' entity='state' />
-    </>
-
-const sorts = [
-    {
-        caption: "Newest",
-        column: "date",
-        direction: "desc"
-    },
-    {
-        caption: "Most important",
-        key: "MostImportant"
-    }
-]
-
-const listActions = (itemIds) => {
-
-    const closeAll = ({ setProgress, reloadList }) => {
-        setProgress(true);
-        post('/blog/closeAll', itemIds).then(data => {
-            app.success('Blogs are closed successfully');
-            setProgress(false);
-            reloadList();
-        }, error => {
-            app.error(error);
-            setProgress(false);
-        })
-    }
-
-    return <>
-        <ListAction
-            title='Close all'
-            icon={DoneIcon}
-            click={(params) => closeAll(params)}
-            minCardinality={2}
-        />
-    </>
-}
-
-const itemActions = (item) => {
-    const closeBlog = ({ setProgress, setItem }) => {
-        setProgress(true);
-        post(`/blog/close?blogId=${item.id}`)
-            .then(data => {
-                app.success('Blog is closed');
-                setProgress(false);
-                setItem(data);
-            }, error => {
-                app.error(error);
-                setProgress(false);
-            });
-    }
-
-    return <>
-        <ItemAction
-            title='View'
-            icon={<MessageIcon />}
-            goTo={`/blog/view?blogId=${item.id}`}
-        />
-        {
-            item.stateKey === 'Closed'
-                ?
-                null
-                :
-                <ItemAction
-                    title='Close'
-                    icon={DoneIcon}
-                    click={(params) => closeBlog(params)}
-                />
-        }
-    </>
-}
 
 const headers =
     <>
         <th>#</th>
-        <th> User </th>
         <th>Title</th>
-        <th>Creation date</th>
-        <th>State</th>
     </>
 
 const row = (item) => {
@@ -102,20 +17,7 @@ const row = (item) => {
         <td>{item.id}</td>
         <td>{item.user}</td>
         <td>{item.title}</td>
-        <td>
-            <ValueWithTitle
-                value={new Date(item.date).toDateString()}
-                title={item.relatedItems.TimeAgo + ' ago'}
-            />
-        </td>
-        <td>{item.stateKey}</td>
     </>
-}
-
-const card = (item) => {
-    return <div className="blog bg-orange-200 m-2">
-        <div>{item.title}</div>
-    </div>
 }
 
 const breadcrumbItems = [
@@ -139,15 +41,10 @@ const Blogs = (props) => {
         <List
             title="Blogs"
             entity="blog"
-            filters={filters}
-            sorts={sorts}
-            listActions={listActions}
             headers={headers}
             row={row}
             create={CreateBlog}
-            itemActions={itemActions}
         />
     );
 }
-
 export default Blogs;
