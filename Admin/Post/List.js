949702fd5fa3a7b5c5ac1a9b143ecdcda4117599
Saeed Nemati <sn.1361@gmail.com>
2022-01-22 15:19:18 +0330

    committed by script

--- Admin/Post/List.js
+++ Admin/Post/List.js
@@ -1,5 +1,37 @@
+import { List, Text, Enum } from '@List'
+
+const filters = <>
+    <Text
+        column='Title'
+        placehodler='Title'
+    />
+    <Enum
+        column='StateId'
+        placeholder='State'
+        entityType='postState'
+    />
+</>
+
+const headers = <>
+    <th>Title</th>
+    <th>Date</th>
+    <th>State</th>
+</>
+
+const row = (item) => <>
+    <td>{item.title}</td>
+    <td>{item.date}</td>
+    <td>{item.state}</td>
+</>
+
 const Posts = () => {
-    return <div>Posts</div>
+    return <List
+        title='Posts'
+        entityType='Post'
+        filters={filters}
+        headers={headers}
+        row={row}
+    />
 }
 
 export default Posts
