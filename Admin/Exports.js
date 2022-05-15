dec9d9e10d8e3c5ba7ffcd92ea5fff463e717d58
Saeed Nemati <sn.1361@gmail.com>
2022-05-15 09:42:12 +0430

    committed by script

--- Admin/Exports.js
+++ Admin/Exports.js
@@ -1,5 +1,6 @@
 import { BlogPosts } from './Post/List'
 import PostContent from './Post/Content'
+import ArticleIcon from '@mui/icons-material/Article';
 
 const BlogRoutes = [
     {
@@ -12,5 +13,31 @@ const BlogRoutes = [
     }
 ]
 
+const BlogMenu = [
+    {
+        "title": "Blog",
+        "icon": ArticleIcon,
+        "children": [
+            {
+                "title": "Posts",
+                "url": "/posts"
+            },
+            {
+                "title": "Comments",
+                "url": "/comments?entityType=blogPost"
+            },
+            {
+                "title": "Categories",
+                "url": "/hierarchies?entityType=blogPost"
+            },
+            {
+                "title": "Tags",
+                "url": "/tags?entityType=blogPost"
+            }
+        ]
+    }
+]
+
 export { BlogRoutes }
 export { BlogPosts }
+export { BlogMenu }
