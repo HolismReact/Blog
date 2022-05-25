5a69f0070f840d00def8170671c90febcd84fb9b
Saeed Nemati <sn.1361@gmail.com>
2022-05-25 09:20:56 +0430

    committed by script

--- Admin/Exports.js
+++ Admin/Exports.js
@@ -1,38 +1,43 @@
+import ArticleIcon from '@mui/icons-material/Article';
+import BlogHierarchies from './Hierarchy/Tree'
 import { BlogPosts } from './Post/List'
 import PostContent from './Post/Content'
-import ArticleIcon from '@mui/icons-material/Article';
 
 const BlogRoutes = [
     {
-        "path": "/posts",
-        "component": BlogPosts
+        path: "/posts",
+        component: BlogPosts
     },
     {
-        "path": "/post/editContent",
-        "component": PostContent
+        path: "/post/editContent",
+        component: PostContent
+    },
+    {
+        path: "/blogCategories",
+        component: BlogHierarchies
     }
 ]
 
 const BlogMenu = [
     {
-        "title": "Blog",
-        "icon": ArticleIcon,
-        "children": [
+        title: "Blog",
+        icon: ArticleIcon,
+        children: [
             {
-                "title": "Posts",
-                "url": "/posts"
+                title: "Posts",
+                url: "/posts"
             },
             {
-                "title": "Comments",
-                "url": "/comments?entityType=blogPost"
+                title: "Comments",
+                url: "/comments?entityType=blogPost"
             },
             {
-                "title": "Categories",
-                "url": "/blogCategories"
+                title: "Categories",
+                url: "/blogCategories?entityType=blogPost"
             },
             {
-                "title": "Tags",
-                "url": "/tags?entityType=blogPost"
+                title: "Tags",
+                url: "/tags?entityType=blogPost"
             }
         ]
     }
--- Admin/Hierarchy/Tree.js
+++ Admin/Hierarchy/Tree.js
@@ -3,7 +3,6 @@ import { HierarchyTree } from "../../Taxonomy/Hierarchy/Tree";
 const BlogHierarchies = () => {
     return <HierarchyTree
         title="Blog Categories"
-        entityType='BlogPost'
     />
 }
 
